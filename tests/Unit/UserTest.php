82d659804d2c46866e5625914455c9c5eadaf362
halilcakar <hcakar.1992@gmail.com>
2021-06-25 21:10:09 +0300

    Episode 33 - Admin Set Status Test

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_check_if_user_is_an_admin()
+    {
+        $user = User::factory()->create(['email' => 'hcakar.1992@gmail.com' ]);
+        $userB = User::factory()->create([]);
+
+        $this->assertTrue($user->isAdmin());
+        $this->assertFalse($userB->isAdmin());
+    }
+}
